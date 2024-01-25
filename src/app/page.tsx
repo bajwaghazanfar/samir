ae531fa947e2434d96004eb75742cdd866228d19
Ghazanfar <bajwaghazanfar9@gmail.com>
2024-01-25 12:05:45 +0000

    removed apos error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -141,14 +141,14 @@ export default function Home() {
             <div className="w-full h-full flex flex-col justify-center  items-center gap-5 lg:flex-row ">
               <div className="w-auto h-auto  bg-violet-200 p-5 rounded-xl">
                 <p className="text-sm text-black text-pretty tracking-wide ">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
                   irure dolor in reprehenderit in voluptate velit esse cillum
                   dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                   cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum."
+                  mollit anim id est laborum.
                 </p>
               </div>
             </div>
@@ -272,7 +272,7 @@ export default function Home() {
               </h1>
               <div className="w-full h-auto flex justify-start items-start flex-col gap-3">
                 <p className="text-sm lg:text-base text-gray-500">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
@@ -302,7 +302,7 @@ export default function Home() {
               </h1>
               <div className="w-full h-auto flex justify-start items-start flex-col gap-3">
                 <p className="text-sm lg:text-base text-gray-500">
-                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                   Ut enim ad minim veniam, quis nostrud exercitation ullamco
                   laboris nisi ut aliquip ex ea commodo consequat. Duis aute
