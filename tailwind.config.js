40a43a32065a8b839a9f578692c3306a1c334b6c
Ghazanfar <bajwaghazanfar9@gmail.com>
2024-01-30 15:28:26 +0000

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,66 +1,48 @@
 
 
-// module.exports = {
-//   important:true,
-//   content: [
-//     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-//   ],
-//   theme: {
-//     extend: {
-//       screens: {
-//         "3xl": "2000px",
-//       },
-//       colors: {
-//         secondary:"#64B5F6"
-//       },
-//       fontFamily: {
-//                     "merriweather": "var(--font-merriweather)", // note: you can call the left side of this whatever you want - barlow-bold or title-font or foo-bar, this is what you'll use in your Tailwind css classes to use this font
-//                     "poppins": "var(--font-poppins)", // note: the bit that goes inside the var() function is the same variable name we defined in app.tsx
-//       },
-//       backgroundImage: (theme) => ({
-//         abstract:
-//           "url(/bg.png)",
-//           abstract_wide:
-//           "url(/bg-wide.png) ",
-//           city:  "url(/bg-city.jpg) ",
-//       }),
-//       boxShadow: {
-//         oval: "0px 0px 38px -3px #EBF7FF;",
-//       },
-//       screens: {
-//         "3xl": "2000px",
-//         // => @media (min-width: 2000px) { ... }
-//       },
-//     },
-//   },
-//   plugins: [
-//     require('@tailwindcss/typography'),
-//     require('@tailwindcss/forms'),
-//     require('@tailwindcss/aspect-ratio'),
-//     require('daisyui'),
-//   ],
-//     daisyui: {
-//       themes: [], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-//     base: true, // applies background color and foreground color for root element by default
-//     styled: true, // include daisyUI colors and design decisions for all components
-//     utils: true, // adds responsive and modifier utility classes
-//     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-//     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-//     themeRoot: ":root", 
-//     }
-// };
 module.exports = {
+  important:true,
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [require('daisyui')],
-  daisyui: {
-    themes: [
-      "light",
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      },
+      colors: {
+        secondary:"#64B5F6"
+      },
+      fontFamily: {
+                    "merriweather": "var(--font-merriweather)", // note: you can call the left side of this whatever you want - barlow-bold or title-font or foo-bar, this is what you'll use in your Tailwind css classes to use this font
+                    "poppins": "var(--font-poppins)", // note: the bit that goes inside the var() function is the same variable name we defined in app.tsx
+      },
+      backgroundImage: (theme) => ({
+        abstract:
+          "url(/bg.png)",
+          abstract_wide:
+          "url(/bg-wide.png) ",
+          city:  "url(/bg-city.jpg) ",
+      }),
+      boxShadow: {
+        oval: "0px 0px 38px -3px #EBF7FF;",
+      },
+      screens: {
+        "3xl": "2000px",
+        // => @media (min-width: 2000px) { ... }
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('daisyui'),
+  ],
+    daisyui: {
+      themes: [  "light",
       "dark",
       "cupcake",
       "bumblebee",
@@ -91,7 +73,12 @@ module.exports = {
       "winter",
       "dim",
       "nord",
-      "sunset",
-    ],
-  },
-}
+      "sunset",], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", 
+    }
+};
