f7a339fa39df6d3058c1c1917c9e5d1fbbeb0755
Ghazanfar <bajwaghazanfar9@gmail.com>
2024-01-30 14:34:43 +0000

    added default daisyui navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,11 +39,6 @@ module.exports = {
   },
   plugins: [require("daisyui")],
     daisyui: {
-      styled: true,
-      themes: true,
-      base: true,
-      utils: true,
-      logs: true,
-      rtl: false
+     
     }
 };
