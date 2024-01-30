68d281d966e70af6713fb3857210cd5bf5bf27fa
Ghazanfar <bajwaghazanfar9@gmail.com>
2024-01-30 14:40:53 +0000

    lets ee if if it works

--- Components/Navbar/index.tsx
+++ Components/Navbar/index.tsx
@@ -7,7 +7,7 @@ import DottedButton from "../Button/Button";
 export const Navbar = () => {
   const [open, setOpen] = useState<boolean>(false);
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 w-full">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -19,9 +19,9 @@ export const Navbar = () => {
               stroke="currentColor"
             >
               <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
                 d="M4 6h16M4 12h8m-8 6h16"
               />
             </svg>
@@ -31,51 +31,64 @@ export const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a>Item 1</a>
+              <a href="/">Home</a>
             </li>
             <li>
-              <a>Parent</a>
+              <a>Case Studies</a>
               <ul className="p-2">
                 <li>
-                  <a>Submenu 1</a>
+                  <a href="/work">All case studies</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a href="/casestudy/sportband">Active core</a>
                 </li>
               </ul>
             </li>
-            <li>
-              <a>Item 3</a>
-            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className=" w-fit" href="/">
+          <img src="/logo.png" className="w-[180px]" />
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
+        <ul className="menu menu-horizontal px-1 relative z-[100]">
           <li>
             <details>
-              <summary>Parent</summary>
+              <summary>Case Studies</summary>
               <ul className="p-2">
                 <li>
-                  <a>Submenu 1</a>
+                  <a href="/work">All case studies</a>
                 </li>
                 <li>
-                  <a>Submenu 2</a>
+                  <a href="/casestudy/sportband">ActiveCore</a>
                 </li>
               </ul>
             </details>
           </li>
-          <li>
-            <a>Item 3</a>
-          </li>
         </ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <DottedButton
+          animation={false}
+          inViewportView={false}
+          id="#ContactForm"
+        >
+          <p className="w-auto flex gap-2 ">
+            Book a call
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm4.28 10.28a.75.75 0 0 0 0-1.06l-3-3a.75.75 0 1 0-1.06 1.06l1.72 1.72H8.25a.75.75 0 0 0 0 1.5h5.69l-1.72 1.72a.75.75 0 1 0 1.06 1.06l3-3Z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </p>
+        </DottedButton>
       </div>
     </div>
   );
